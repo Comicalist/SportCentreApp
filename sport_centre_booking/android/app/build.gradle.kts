8ff3d1716d258e0f0b15cd96b1303db17de88a08
Leon <l.ehrwein@gmail.com>
2025-10-30 14:18:23 +0200

    chore: Update version code and name to 1.0.5+6 for release

--- sport_centre_booking/android/app/build.gradle.kts
+++ sport_centre_booking/android/app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         applicationId = "com.sportcentre.booking"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.4"
     }
 
     buildTypes {
